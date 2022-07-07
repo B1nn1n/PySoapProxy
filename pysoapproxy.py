6d1ba4da88307dfd27a10e86c8b56aa0b2bc8ca8
b1nn1n <jjmooremeister@gmail.com>
2022-07-07 09:30:30 -0500

    Update pysoapproxy.py
    Cleaning up code

--- pysoapproxy.py
+++ pysoapproxy.py
@@ -1,40 +1,46 @@
 import requests
 
-proxies = {
-   'http': 'http://127.0.0.1:8080',
-   'https': 'http://127.0.0.1:8080',
-}
+def main():
+   proxies = {
+      'http': 'http://127.0.0.1:8080',
+      'https': 'http://127.0.0.1:8080',
+   }
 
-# SOAP request URL
-url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
+   # SOAP request URL
+   url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
 
-#read file
-file = open("./soap.file", "r")
+   #read file
+   file = open("./soap.file", "r")
 
-#read the contents of the file, this is the SOAP envelope
-payload = file.read()
+   #read the contents of the file, this is the SOAP envelope
+   payload = file.read()
 
-#close the file
-file.close()
+   #close the file
+   file.close()
 
-print(payload)
+   print(payload)
 
-# structured XML
-#payload = """<?xml version=\"1.0\" encoding=\"utf-8\"?>
-#            <soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">
-#                <soap:Body>
-#                    <CountryIntPhoneCode xmlns=\"http://www.oorsprong.org/websamples.countryinfo\">
-#                        <sCountryISOCode>IN</sCountryISOCode>
-#                    </CountryIntPhoneCode>
-#                </soap:Body>
-#            </soap:Envelope>"""
-# headers
-headers = {
-    'Content-Type': 'text/xml; charset=utf-8'
-}
-# POST request
-response = requests.request("POST", url, headers=headers, data=payload, proxies=proxies)
+   # structured XML
+   #payload = """<?xml version=\"1.0\" encoding=\"utf-8\"?>
+   #            <soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">
+   #                <soap:Body>
+   #                    <CountryIntPhoneCode xmlns=\"http://www.oorsprong.org/websamples.countryinfo\">
+   #                        <sCountryISOCode>IN</sCountryISOCode>
+   #                    </CountryIntPhoneCode>
+   #                </soap:Body>
+   #            </soap:Envelope>"""
+   # headers
+   headers = {
+       'Content-Type': 'text/xml; charset=utf-8'
+   }
+   # POST request
+   response = requests.request("POST", url, headers=headers, data=payload, proxies=proxies)
 
-# prints the response
-print(response.text)
-print(response)
+   # prints the response
+   print(response.text)
+   print(response)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
